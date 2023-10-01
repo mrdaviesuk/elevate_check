539c14b26341abb223f49a568f1e698981c75943
mrdaviesuk <52525875+mrdaviesuk@users.noreply.github.com>
2023-10-01 15:17:59 +0100

    master

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-This tool automates the discovery of "allowElevate"==True in exe file manifests. It's usually used in privilege escalation scenarios.
+This tool automates the discovery of UAC Elevation behaviors defined within Windows Executable (.exe) file manifests. 
+It is forked from the original elevate_check.py to broaden the scope of it's capability and associated utility. 
 
+It may be used in UAC privilege escalation scenarios to identify where "allowElevate"==True, as was the original purpose of the script
 For more detailed info read:  [Bypassing Windows User Account Control (UAC) and ways of mitigation](https://www.greyhathacker.net/?p=796) 
 
+It may also be used to describe or search for the requestedExecution (asInvoker,highestAvailable,requireAdministrator) of an executable, which informs the behavior of the execution for Windows Administrators and Standard Users.
     
 ### Requirements
 -----------------------------
 * python 2.7.x
 * pefile
 * tabulate 
-* BeautifulSoup
-
+* BeautifulSoup4
     
 ### Compatibility
 -----------------------------
@@ -18,7 +20,7 @@ For more detailed info read:  [Bypassing Windows User Account Control (UAC) and 
 	
 ### Usage
 -----------------------------
-usage: elevate_check.py [-h] [-d DIRECTORY] [-r] [-i]
+usage: elevate_check.py [-h] [-d DIRECTORY] [-r] [-i] [-la] [-le] [-el] [-su]
 
 optional arguments:
   -h, show this help message and exit
@@ -28,6 +30,14 @@ optional arguments:
   -r&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Scan subfolders as fell.
   
   -i&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ignore files manufactured by Microsoft.
+
+  -la&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Identify files with the allowElevate parameter set.
+
+  -le&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;List files with the executionLevel setting and the value configured. Can be combined with the -el parameter to limit the scope to a specific executionLevel value or values.
+
+  -el&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Specify a filter for the executionLevel(s) as a comma separated list.
+  
+  -su&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Shows the uiacess setting in the executionLevel output table.
 
 ### Output
 ----------------------------
--- elevate_check.py
+++ elevate_check.py
@@ -1,24 +1,29 @@
 import os
 import platform
 import sys
-import pefile
+import pefile 
 import argparse
-from tabulate import tabulate
-from BeautifulSoup import BeautifulSoup as BS
-from BeautifulSoup import Comment
+import warnings
+from tabulate import tabulate  
+from bs4 import BeautifulSoup as BS
+from bs4 import Comment
 from fnmatch import fnmatch
 
-
+# Forked to create a utility with broader purpose to identify Auto-elevate and UAC Invocation 
 # READ https://www.greyhathacker.net/?p=796
 
-__author__ = 'Dejan Levaja'
-__email__ = 'dejan[@]levaja.com'
+__author__ = 'Paul Davies'
+__email__ = 'dev@mr-davies.co.uk'
+__originalauthor__ = 'Dejan Levaja - dejan[@]levaja.com'
 __license__ = 'GPLv2'
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 ver = platform.platform()
-table = []
+autotable = []
+exectable = []
 
+# XML Parsing for Manufist is preferable but requires lxml - so HTML parser is used but throws warnings which need to be suppressed
+warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
 def list_all_files(root, pattern, recursive):
     allfiles = []
@@ -36,24 +41,62 @@ def list_all_files(root, pattern, recursive):
 
 def get_data(manifest, fname, ignore_ms):
     if manifest:
-        soup = BS(manifest)
+        soup = BS(manifest, "html.parser")
         elevator = soup.find('autoelevate')
-        if elevator:
-            el= elevator.string
-            if str(el) == 'true':
-                desc = soup.find('description')
-                if desc:
-                    description = desc.string
-                else:
-                    description = ''
-                comments = soup.findAll(text=lambda text: isinstance(text, Comment))
-                for comment in comments:
-                    if "Copyright" in comment:
-                        manufacturer = comment.strip()
-                        if 'microsoft' in manufacturer.lower():
-                            if not ignore_ms:
+        level = soup.find('requestedexecutionlevel')
+        if list_auto == True:
+            if elevator:
+                el= elevator.string 
+                if str(el) == 'true':
+                    desc = soup.find('description')
+                    if desc:
+                        description = desc.string
+                    else:
+                        description = ''
+                    comments = soup.findAll(text=lambda text: isinstance(text, Comment))
+                    for comment in comments:
+                        if "Copyright" in comment:
+                            manufacturer = comment.strip()
+                            if 'microsoft' in manufacturer.lower():
+                                if not ignore_ms:
+                                    text = "%s,%s,%s" % (fname, description, manufacturer)
+                                    autotable.append(text.split(','))
+                            else:
                                 text = "%s,%s,%s" % (fname, description, manufacturer)
-                                table.append(text.split(','))
+                                autotable.append(text.split(','))
+
+        if list_execution == True:
+            if level:
+                rel= level.attrs['level']
+                if level.find('uiaccess'):
+                    ui= level.attrs['uiaccess']
+                else:
+                    ui='Undefined'
+                if rel.lower() in execution_level.lower(): 
+                    desc = soup.find('description')
+                    if desc:
+                        description = desc.string
+                    else:
+                        description = '** No Description Field Present **'
+                    comments = soup.findAll(string=lambda string: isinstance(string, Comment))
+                    for comment in comments:
+                        if "Copyright" in comment:
+                            manufacturer = comment.strip()
+                            if 'microsoft' in manufacturer.lower():
+                                if not ignore_ms:
+                                    if show_uiaccess:
+                                        text = "%s,%s,%s,%s,%s" % (fname, rel, ui, description, manufacturer)
+                                        exectable.append(text.split(','))
+                                    else:
+                                        text = "%s,%s,%s,%s" % (fname, rel, description, manufacturer)
+                                        exectable.append(text.split(','))
+                            else:
+                                if show_uiaccess:
+                                    text = "%s,%s,%s,%s,%s" % (fname, rel, ui, description, manufacturer)
+                                    exectable.append(text.split(','))
+                                else:
+                                    text = "%s,%s,%s,%s" % (fname, rel, description, manufacturer)
+                                    exectable.append(text.split(','))
 
 
 def get_manifest(fname):
@@ -75,27 +118,44 @@ def get_manifest(fname):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--directory', help='Target directory.', default="C:\\Windows\\System32")
-    parser.add_argument('-r', '--recursive', help='Scan subfolders as fell.', action='store_true')
+    parser.add_argument('-r', '--recursive', help='Scan Sub-folders as well.', action='store_false')
     parser.add_argument('-i', '--ignore-ms', help='Ignore files manufactured by Microsoft.', action='store_true')
+    parser.add_argument('-la', '--list-auto', help='List the Auto Elevate Applications', action='store_false')
+    parser.add_argument('-le', '--list-execution', help='List the Requested Execution Level', action='store_false')
+    parser.add_argument('-el', '--execution-level', help='Specify the Execution Level to filter for (Default: asInvoker,highestAvailable,requireAdministrator)', default='asInvoker,highestAvailable,requireAdministrator')
+    parser.add_argument('-su', '--show-uiaccess', help='Show uiaccess value, when outputting Requested Execution Level', action='store_true')
     args = vars(parser.parse_args())
 
     directory = args['directory']
     recursive = args['recursive']
     ignore_ms = args['ignore_ms']
-
-    print '\nPlease wait, it can take some time...'
+    list_auto = args['list_auto']
+    list_execution = args['list_execution']
+    execution_level = args['execution_level']
+    show_uiaccess = args['show_uiaccess']
+    
+    print('\nPlease wait, it can take some time...')
     filenames = list_all_files(directory, "*.exe", recursive)
     total = len(filenames)
 
     # progress
     for i, filename in enumerate(filenames):
-        print '[%s of %s] Processing file "%s"' % (i+1, total, filename)
+        print('[%s of %s] Processing file "%s"' % (i+1, total, filename))
         manifest = get_manifest(filename)
         get_data(manifest, filename, ignore_ms)
 
     # print table
-    print '\n\n[!] Total found: %d' % len(table)
-    headers = ['File', 'Description', 'Manufacturer']
-    print tabulate(table, headers, tablefmt='grid')
+    if len(autotable) != 0:
+        print ('\n\n[!] Total AutoElevate Applications found: %d' % len(autotable))
+        headers = ['File', 'Description', 'Manufacturer']
+        print (tabulate(autotable, headers, tablefmt='grid'))
+
+    if len(exectable) != 0:
+        print ('\n\n[!] Total Applications with Specified Elevation Level found: %d' % len(exectable))
+        if show_uiaccess:
+            headers = ['File', 'Execution Level', 'UIAccess', 'Description', 'Manufacturer']
+        else:
+            headers = ['File', 'Execution Level', 'Description', 'Manufacturer']
+        print (tabulate(exectable, headers, tablefmt='grid'))
 
     sys.exit('\n\n[!] Done.\n\n')
